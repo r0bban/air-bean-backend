969d63ab4fd75568b74e4dca3cd167f61839a5c3
r0bban <robert.p.svensson@gmail.com>
2020-06-01 12:58:28 +0200

    fix for cross origin

---
+++ src/main/java/io/r0bban/airbean/Config/CorsConfig.java
@@ -0,0 +1,27 @@
+package io.r0bban.airbean.Config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
+                config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        return registration;
+    }
+}
