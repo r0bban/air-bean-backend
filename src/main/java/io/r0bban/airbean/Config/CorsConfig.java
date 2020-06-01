108064cedf0e312a3970ff8c180921895dae9532
r0bban <robert.p.svensson@gmail.com>
2020-06-01 13:25:52 +0200

    disabled fix for cross origin, using front fix instead

--- src/main/java/io/r0bban/airbean/Config/CorsConfig.java
+++ src/main/java/io/r0bban/airbean/Config/CorsConfig.java
@@ -9,19 +9,19 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
-                config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-        return registration;
-    }
-}
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilter() {
+//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        final CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
+//                config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+//        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+//        source.registerCorsConfiguration("/**", config);
+//        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+//        return registration;
+//    }
+//}
